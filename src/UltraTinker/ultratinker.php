bfd03d139c7b09b8c8299775dbd147b488de9b88
Tayyab1101 <tayyabisking1101@gmail.com>
2020-09-05 18:20:33 +0500

    modification in exec command path

--- src/UltraTinker/ultratinker.php
+++ src/UltraTinker/ultratinker.php
@@ -6,7 +6,7 @@ if (isset($_POST['input'])) {
     file_put_contents('main.php', $_POST['input']);
 }
 $con = new voku\AnsiConverter\AnsiToHtmlConverter();
-$data = $con->convert(shell_exec('cd ../../../../../;more vendor/back2future/tinkerplus/src/TinkerPlus/main.php | php artisan tinker --ansi'));
+$data = $con->convert(shell_exec('cd ../../../../../;more vendor/back2future/ultratinker/src/UltraTinker/main.php | php artisan tinker --ansi'));
 $data = str_replace('color: blue;', 'color:#2471c8;', $data);
 $data = "<pre>" . str_replace('background-color: black;', '', $data) . "</pre>";
 echo $data;
