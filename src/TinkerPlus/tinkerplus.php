e53d2576b2d0163100f344a5445e40b6ab1aba1e
Tayyab1101 <tayyabisking1101@gmail.com>
2020-09-05 17:14:39 +0500

    fixed tinkerwell

--- src/TinkerPlus/tinkerplus.php
+++ src/TinkerPlus/tinkerplus.php
@@ -1,12 +1,13 @@
 <?php
-include '../vendor/voku/ansi-to-html/src/voku/AnsiConverter/Theme/Theme.php';
-include '../vendor/voku/ansi-to-html/src/voku/AnsiConverter/AnsiToHtmlConverter.php';
+include '../../../../voku/ansi-to-html/src/voku/AnsiConverter/Theme/Theme.php';
+include '../../../../voku/ansi-to-html/src/voku/AnsiConverter/AnsiToHtmlConverter.php';
 
 if (isset($_POST['input'])) {
     file_put_contents('main.php', $_POST['input']);
 }
 $con = new voku\AnsiConverter\AnsiToHtmlConverter();
-$data = $con->convert(shell_exec('cd ../;more tinkerplus/main.php | php artisan tinker --ansi'));
+$data = $con->convert(shell_exec('cd ../../../../../;more vendor/back2future/tinkerplus/src/TinkerPlus/main.php | php artisan tinker --ansi'));
 $data = str_replace('color: blue;', 'color:#2471c8;', $data);
 $data = "<pre>" . str_replace('background-color: black;', '', $data) . "</pre>";
 echo $data;
+
